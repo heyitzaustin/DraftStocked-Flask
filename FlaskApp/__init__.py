b289b672b2d0730a44f9e39efc9a393c3d229748
Austin Li <heyitzaustin@github.com>
2015-05-22 07:24:16 -0400

    Polish

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,9 +1,13 @@
 from flask import *
 import sqlite3
 import datetime
+import time
+
 app = Flask(__name__)
 
 DATABASE = '/var/www/DraftStocked/FlaskApp/Application/redditdata.db'
+secondsDay = 86400
+
 
 
 def get_db():
@@ -42,14 +46,23 @@ def playerpage(name):
 	stock = temp[1]
 	change = temp[2]
 
+	recentList = []
+	otherList = []
+	currentTime = int(time.time())
 	cur.execute("SELECT Score,Body,Permalink,Dateposted FROM Comments WHERE Player=?",(name,))
 	rows = cur.fetchall()
+
 	for row in rows:
 		t = datetime.datetime.fromtimestamp(row[3]).strftime('%m-%d-%Y')
 		if row[0] > 0:
-			commentList.append((row[0],row[1],row[2],t))
-	commentList.sort(key=lambda x: x[0],reverse=True)
-	return render_template('player.html',name=playername,stock=stock,change=change,comments=commentList)
+			if row[3] > currentTime-(2*secondsDay):
+				recentList.append((row[0],row[1],row[2],t))
+			else:
+				otherList.append((row[0],row[1],row[2],t))
+	recentList.sort(key=lambda x: x[0],reverse=True)			
+	otherList.sort(key=lambda x: x[0],reverse=True)
+
+	return render_template('player.html',name=playername,stock=stock,change=change,rcomments=recentList,ocomments=otherList)
 
 if __name__ == "__main__":
     app.run()
